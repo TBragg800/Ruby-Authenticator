e5ec1fe6c5605152786b43c8a245022ddf236e8c
LM&T <tbbragg83@gmail.com>
2021-01-31 17:40:22 -0600

    changed file to a ruby file and added a while loop

--- ruby_authenticator.rb
+++ ruby_authenticator.rb
@@ -10,3 +10,15 @@ puts "Welcome to the authenticator"
 puts
 puts "This program will take input from the user and compare the password"
 puts "If the password is correct you will get back the user object"
+
+x = 4
+while x >= 1
+    print "Username: "
+    username = gets.chomp
+    print "Password: "
+    password = gets.chomp
+    puts "Press n to quit or any other key to continue: "
+    input = gets.chomp.downcase
+    break if input == "n"
+    x -= 1
+end
